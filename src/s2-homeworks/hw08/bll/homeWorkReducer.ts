782431b3f1ce6de59fba6dd0fe23a2628d8a1863
Pavel <pavel07007@gmail.com>
2022-12-26 11:02:27 +0300

    change reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,12 +8,12 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
     switch (action.type) {
         case 'sort': { // by name
             if(action.payload === "up"){
-                return state.sort((x,y)=> x.name.localeCompare(y.name))
+                return [...state.sort((x,y)=> x.name.localeCompare(y.name))]
             }
-            return  state.sort((x,y)=> y.name.localeCompare(x.name))
+            return  [...state.sort((x,y)=> y.name.localeCompare(x.name))]
         }
         case 'check': {
-            return state.filter(el=> el.age > 17).sort((x,y)=> y.name.localeCompare(x.name)) // need to fix
+            return state.filter(el=> el.age > 17) // need to fix
         }
         default:
             return state
