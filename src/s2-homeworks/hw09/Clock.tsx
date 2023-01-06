40eb39316e8aebb1f45a235c6fa8e2eec2e753d5
Pavel <pavel07007@gmail.com>
2023-01-06 22:14:26 +0300

    change  day/mounth 2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -35,7 +35,7 @@ function Clock() {
          month: 'long',
     });
     const day = new Intl.DateTimeFormat("en-US", {
-        day: '2-digit', month: '2-digit', year: 'numeric'
+        month: '2-digit',day: '2-digit', year: 'numeric'
     });
     const dayWeek = new Intl.DateTimeFormat("en-US", {
         weekday:'long'
